f1e3f06544f6556d6080a98d7291ed97b7735df4
raco <cekcar@gmail.com>
2024-08-19 15:43:10 +0200

    Small update

--- ManagerDelegate.cs
+++ ManagerDelegate.cs
@@ -4,14 +4,17 @@ namespace Managers
 {
     public class ManagerDelegate
     {
-        internal readonly ActionWrapperClasses.WrappedAction<string, string> EnableBulletFromPool =
+        internal readonly ActionWrapperClasses.WrappedAction<string, string> Example =
             new ActionWrapperClasses.WrappedAction<string, string>();
     }
     
     //EXAMPLE
     private void Example()
     {
-        EnableBulletFromPool.Register(ExampleAction);
+        //register
+        Example.Register(ExampleAction);
+        //calling
+        Example?.Invoke("1", "2");
     }
 
     private void ExampleAction(string arg1, string arg2)
